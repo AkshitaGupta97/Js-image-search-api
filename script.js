c6850ab0fc45597b6d8c24145e0c270e253e18e7
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-07-22 16:14:20 +0530

    fetching url changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const searchform = document.querySelector('form')
 const imageContainer = document.querySelector('.images-container');
 const searchInput = document.querySelector('.search-input');
 
-const accessKey = '';
+const accessKey = 'o8GIiuJ02Vtm0aiUJid4zUJW1fRAqBAT72XlynGh9l4';
 //o8GIiuJ02Vtm0aiUJid4zUJW1fRAqBAT72XlynGh9l4
 
  // link https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
@@ -12,12 +12,30 @@ const accessKey = '';
 
 const fetchImages = async (image) => {
   //  console.log(image);
-    
-  const url =` https://api.unsplash.com/photos/?query=${image}&per_page=28&client_id=${accessKey}`; // query=${image}&per_page=28 -> here per_page = 28 means we will get 28 images in each page.
+    imageContainer.innerHTML = '';
+
+  const url =` https://api.unsplash.com/search/photos/?query=${image}&per_page=30&client_id=${accessKey}`; // we use search becuse i need to fetch those images which i am seaeching.
+  //  query=${image}&per_page=28 -> here per_page = 28 means we will get 30 images in each page.
+
   const response = await fetch(url);
   const data = await response.json();
 
-  console.log(data);
+ // console.log(data);
+
+    data.results.forEach(photo => {   // as images are wraped in array of results.
+        const imageItem = document.createElement('div');
+        imageItem.classList.add('image-div')
+
+        imageItem.innerHTML = `<img src="${photo.urls.regular}">`;  // the {photo.urls.regular} i am getting it from apis. in console data
+        
+        const overlay = document.createElement('div')
+        overlay.classList.add('overlay');
+
+        imageItem.appendChild(overlay);
+
+        imageContainer.appendChild(imageItem);
+    });
+
   
 
 
